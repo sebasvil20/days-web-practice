b4305b703bb0fa4a888183e5ef443e839b23e662
Sebastian Villegas <sebasvil20@gmail.com>
2021-02-15 10:53:28 -0500

    Adding BarbaJs for page loading transitions

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let controller;
 let slideScene;
 let pageScene;
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-header");
 
 function animateSlides() {
   //Init controller
   controller = new ScrollMagic.Controller();
-
   const sliders = document.querySelectorAll(".slide");
-  const nav = document.querySelector(".nav-header");
 
   //Loop over each slide
   sliders.forEach((slide, index, slides) => {
@@ -76,4 +78,114 @@ function animateSlides() {
   });
 }
 
-animateSlides();
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, {
+      y: "0%",
+    });
+    mouseText.innerText = "Tap";
+  } else {
+    mouse.classList.remove("explore-active");
+
+    gsap.to(".title-swipe", 1, {
+      y: "100%",
+    });
+    mouseText.innerText = "";
+  }
+}
+
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+
+//BarbaJs page transitions
+barba.init({
+  views: [
+    {
+      namespace: "home",
+      beforeEnter() {
+        animateSlides();
+        logo.href = "./index.html";
+      },
+      beforeLeave() {
+        slideScene.destoy();
+        pageScene.destroy();
+        controller.destroy();
+      },
+    },
+    {
+      namespace: "fashion",
+      beforeEnter() {
+        logo.href = "../index.html";
+        gsap.fromTo(
+          ".nav-header",
+          1,
+          { y: "100%", y: "0%" },
+          { y: "0%", ease: "power2.inOut" }
+        );
+      },
+    },
+  ],
+  transitions: [
+    {
+      leave({ current, next }) {
+        let done = this.async();
+        //An animation
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+        tl.fromTo(
+          ".swipe",
+          0.75,
+          { x: "-100%" },
+          { x: "0%", onComplete: done },
+          "-=0.5"
+        );
+      },
+      enter({ current, next }) {
+        let done = this.async();
+        //Scroll to the top
+        window.scrollTo(0, 0);
+        //An animation
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(
+          ".swipe",
+          1,
+          { x: "0" },
+          { x: "100%", stagger: 0.25, onComplete: done }
+        );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+      },
+    },
+  ],
+});
+
+//Event listeners
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
+burger.addEventListener("click", navToggle);
